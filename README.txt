aac4784a7895ba31a684b6cf531f668896e1d117
kevin <googlevsbing@126.com>
2017-11-30 19:03:02 +0900

    django learning

---
+++ README.txt
@@ -0,0 +1,17 @@
+
+django starting tuturial learning
+
+
+django-admin startproject onenew
+python manage.py runserver
+
+python manage.py startapp polls
+
+add models 
+python manage.py makemigrations polls
+
+python manage.py sqlmigrate polls 0001    (it's test , do not affect db)
+
+python manage.py check (it's test , do not affect db)
+
+python manage.py migrate
---
+++ myproject/polls/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from .models import Question 
+
+def index(request):
+
+	latest_question_list = Question.objects.order_by("pub_date")[:5]
+	template = loader.get_template("polls/index.html")
+	context = {
+		"latest_question_list": latest_question_list
+	}
+	return HttpResponse(template.render(context,request))
+
+
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk= question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+
+		return render(request,"polls/detail.html",{"question":question})
+	# return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're loooking at the results of question %s."
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
+
+
